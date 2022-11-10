32bb9fb042dcefbe21b865dc4e69d2030a1dd7f8
Matheus Alves de Araujo <101357346+AThews@users.noreply.github.com>
2022-11-10 14:38:29 -0300

    div quase configurada

--- relogio.js
+++ relogio.js
@@ -3,13 +3,8 @@
 const hora = new Date().toLocaleTimeString();
 
 //Pegar a div e o corpo do html
-let div = document.getElementById('foto');
-let corpo = document.body()
-
 
 //dizer que horas são
-
-div.innerHTML = `Agora são ${hora}.`
 
 //mudar a foto de acordo com a hora
 
